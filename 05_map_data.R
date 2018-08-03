de1726cd59a6a9fb632aa01e680791adb59c201d
Julie <padilla410@gmail.com>
2018-08-03 11:10:47 -0400

    adding data objects to avoid loading scripts

---
+++ 05_map_data.R
@@ -0,0 +1,3 @@
+save(pm_stats, stations, file = "out/data/pm_stats.RData")
+save(airzone_map, stations, file = "out/data/airzone_map.RData")
+save(airzones_annual_mgmt, stations, file = "out/data/airzones_annual_mgmt.RData")
